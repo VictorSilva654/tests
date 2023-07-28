273a22e3e96072990af0677f6bdc70dd9c1982d1
VictorSilva654 <victordejesus654@gmail.com>
2023-07-28 20:27:11 -0300

    fazendo script de testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,26 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve adicionar dois comentários ao DOM', () => {
+        render(<PostComment/>)
+
+        fireEvent.change(screen.getByTestId('textoComentario'), {
+            target: {
+                value: 'Iserindo um primeiro comentário' 
+            }
+        })
+        fireEvent.click(screen.getByTestId('botaoComentario'))
+
+        fireEvent.change(screen.getByTestId('textoComentario'), {
+            target: {
+                value: 'INserindo um segundo comentário' 
+            }
+        })
+        fireEvent.click(screen.getByTestId('botaoComentario'))
+
+        expect(screen.getAllByTestId('elementoComentario')).toHaveLength(2)
+
+    })
 });
+
